36237b88881a2e49aa143055a313fd48069547c0
Cristian Gonzalez <contact@cristianngonzalez.com>
2023-03-11 22:14:42 -0300

    New admin home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -13,8 +13,8 @@
                     <img src="..." class="card-img-top" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Blogs</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                      <p class="card-text">You can edit, delete, and update your blogs and it's categories.</p>
+                      <a href="#" class="btn btn-primary">Manage blogs</a>
                     </div>
                 </div>
             </div>
@@ -25,7 +25,7 @@
                     <div class="card-body">
                       <h5 class="card-title">Users</h5>
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                      <a href="#" class="btn btn-primary">Manage users</a>
                     </div>
                 </div>
             </div>
@@ -36,7 +36,7 @@
                     <div class="card-body">
                       <h5 class="card-title">Social networks</h5>
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                      <a href="#" class="btn btn-primary">Manage socials networks</a>
                     </div>
                 </div>
             </div>
