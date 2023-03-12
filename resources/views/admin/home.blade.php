3be6a7450cdbb5e8399eac5fb7677aba35fdcbfe
Cristian Gonzalez <contact@cristianngonzalez.com>
2023-03-11 22:48:58 -0300

    New admin home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -8,46 +8,82 @@
     <div class="container">
         <div class="row">
 
-            <div class="col-md-3">
+            <div class="col-md-3 mb-2">
                 <div class="card" style="width: 100%;">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="{{env('ASSETS_URL')}}img/svg/icons/blogs.svg" class="card-img-top" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Blogs</h5>
                       <p class="card-text">You can edit, delete, and update your blogs and it's categories.</p>
-                      <a href="#" class="btn btn-primary">Manage blogs</a>
                     </div>
                 </div>
             </div>
 
-            <div class="col-md-3">
+            <div class="col-md-3 mb-2">
                 <div class="card" style="width: 100%;">
-                    <img src="https://icons.getbootstrap.com/icons/newspaper/" class="card-img-top" alt="...">
+                    <img src="{{env('ASSETS_URL')}}img/svg/icons/users.svg" class="card-img-top" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Users</h5>
                       <p class="card-text">Your site has users, as admin you can set and remove them as admin.</p>
-                      <a href="#" class="btn btn-primary">Manage users</a>
                     </div>
                 </div>
             </div>
 
-            <div class="col-md-3">
+            <div class="col-md-3 mb-2">
                 <div class="card" style="width: 100%;">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="{{env('ASSETS_URL')}}img/svg/icons/linkedin.svg" class="card-img-top" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Social networks</h5>
                       <p class="card-text">You can edit your social networks links.</p>
-                      <a href="#" class="btn btn-primary">Manage socials networks</a>
                     </div>
                 </div>
             </div>
 
-            <div class="col-md-3">
+            <div class="col-md-3 mb-2">
                 <div class="card" style="width: 100%;">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="{{env('ASSETS_URL')}}img/svg/icons/content.svg" class="card-img-top" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Static content</h5>
                       <p class="card-text">Your website has titles, paragraphs and others statics content that you can edit here.</p>
-                      <a href="#" class="btn btn-primary">Manage your static content</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-2">
+                <div class="card" style="width: 100%;">
+                    <img src="{{env('ASSETS_URL')}}img/svg/icons/messages.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Messages</h5>
+                      <p class="card-text">Your site has a contact form where you can receive messages, and you can send message from your admin here.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-2">
+                <div class="card" style="width: 100%;">
+                    <img src="{{env('ASSETS_URL')}}img/svg/icons/cv_request.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Cv requests</h5>
+                      <p class="card-text">You can admin who want to have your cv and sent it.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-2">
+                <div class="card" style="width: 100%;">
+                    <img src="{{env('ASSETS_URL')}}img/svg/icons/timeline.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Timeline</h5>
+                      <p class="card-text">You can admin who want to have your cv and sent it.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-2">
+                <div class="card" style="width: 100%;">
+                    <img src="{{env('ASSETS_URL')}}img/svg/icons/testimonials.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Testimonials</h5>
+                      <p class="card-text">You can admin who want to have your cv and sent it.</p>
                     </div>
                 </div>
             </div>
