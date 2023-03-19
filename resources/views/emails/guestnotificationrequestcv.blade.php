e5d84a4363ce3684af9fcedcc9e3e6c8525d710d
Cristian Gonzalez <contact@cristianngonzalez.com>
2023-03-19 02:49:46 -0300

    Messages

--- resources/views/emails/guestnotificationrequestcv.blade.php
+++ resources/views/emails/guestnotificationrequestcv.blade.php
@@ -1,6 +1,6 @@
 @include('emails.partials.header')
 
-<div style="padding-left: 20px; padding-right:20px; min-height: 300px;  max-width: 500px;  margin-left: auto; margin-right: auto;">
+<div style="padding-left: 20px; padding-right:20px; min-height: 300px;  max-width: 500px;  margin-left: auto; margin-right: auto; ">
     
     <div style="padding-left: 20px; padding-right:20px ;">
         <p>Muchas gracias por su interés, usted ha solicitado mi cv, te responderé lo más breve posible.</p>
