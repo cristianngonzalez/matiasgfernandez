ce98c6f6dc0602dcf25ae897dd214f2a73af8b81
Cristian Gonzalez <contact@cristianngonzalez.com>
2023-03-22 19:40:00 -0300

    Testimonials

--- resources/views/admin/blogedit.blade.php
+++ resources/views/admin/blogedit.blade.php
@@ -47,6 +47,11 @@
                             <label for="category">Category</label>
                         </div>
 
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+
                         <div class="form-floating mb-3">
                             <input type="file" class="form-control" name="picture-file" id="picture-file">
                             <label for="picture-file">Picture file</label>
